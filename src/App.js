2ded16f81411ab2ba6c52e9c51a6edbd3383e3af
kartikgits <kartik.kapparwan1@gmail.com>
2020-01-09 21:08:37 +0530

    Add New Contact Button -Broken URL

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import CreateContact from './CreateContact.js'
 class App extends Component {
   state = {
     contacts: [],
-    screen: 'create'
+    screen: 'list'
   }
 
   componentDidMount() {
@@ -36,6 +36,11 @@ class App extends Component {
           <ListContacts
             contacts={this.state.contacts}
             onDeleteContact={this.removeContact}
+            onNavigate={() => {
+              this.setState(() => ({
+                screen: 'create'
+              }))
+            }}
           />
           )
         }
